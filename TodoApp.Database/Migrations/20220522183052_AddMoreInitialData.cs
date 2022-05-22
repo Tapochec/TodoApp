0cf741a69d4369881dd6c215938279d7e98a3fec
Andrew <and_petr_00@mail.ru>
2022-05-22 21:32:38 +0300

    Добавил больше начальных данных

---
+++ TodoApp.Database/Migrations/20220522183052_AddMoreInitialData.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp.Database.Migrations
+{
+    public partial class AddMoreInitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ListGroups",
+                columns: new[] { "Id", "Name", "UserId" },
+                values: new object[] { 1, "Group 1", 1 });
+
+            migrationBuilder.InsertData(
+                table: "TaskLists",
+                columns: new[] { "Id", "Name", "TaskGroupId", "UserId" },
+                values: new object[] { 2, "List2", 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "Id", "CompletionDt", "Desc", "Name", "TaskListId" },
+                values: new object[] { 4, null, "Desc4", "Task4", 2 });
+
+            migrationBuilder.InsertData(
+                table: "Tasks",
+                columns: new[] { "Id", "CompletionDt", "Desc", "Name", "TaskListId" },
+                values: new object[] { 5, null, "Desc5", "Task5", 2 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Tasks",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "TaskLists",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ListGroups",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
--- TodoApp.Database/Models/TodoDbContext.cs
+++ TodoApp.Database/Models/TodoDbContext.cs
@@ -24,20 +24,28 @@ namespace TodoApp.Database.Model
                 .Property(b => b.Completed)
                 .HasDefaultValue(false);
 
-            // Данные инициализации
+            #region Начальные данные
             modelBuilder.Entity<User>().HasData(
                 new User { Id = 1, Email = "andrew@example.com", Password = "qwe" }
             );
 
+            modelBuilder.Entity<ListGroup>().HasData(
+                new ListGroup { Id = 1, UserId = 1, Name = "Group 1" }
+            );
+
             modelBuilder.Entity<TaskList>().HasData(
-                new TaskList { Id = 1, UserId = 1, Name = "List1" }
+                new TaskList { Id = 1, UserId = 1, Name = "List1" },
+                new TaskList { Id = 2, UserId = 1, TaskGroupId = 1, Name = "List2" }
             );
 
             modelBuilder.Entity<Task>().HasData(
                 new Task { Id = 1, TaskListId = 1, Name = "Task1", Desc = "Desc1" },
                 new Task { Id = 2, TaskListId = 1, Name = "Task2", Desc = "Desc2" },
-                new Task { Id = 3, TaskListId = 1, Name = "Task3", Desc = "Desc3" }
+                new Task { Id = 3, TaskListId = 1, Name = "Task3", Desc = "Desc3" },
+                new Task { Id = 4, TaskListId = 2, Name = "Task4", Desc = "Desc4" },
+                new Task { Id = 5, TaskListId = 2, Name = "Task5", Desc = "Desc5" }
             );
+            #endregion
         }
     }
 }
